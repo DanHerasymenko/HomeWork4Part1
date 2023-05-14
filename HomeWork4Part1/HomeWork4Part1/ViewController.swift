b70cfa88aa54c70ff869f0fa27634604faa8fbac
Daniil <45711483+DanHerasymenko@users.noreply.github.com>
2023-05-14 14:47:09 +0300

    completed all tasks

--- HomeWork4Part1/HomeWork4Part1/ViewController.swift
+++ HomeWork4Part1/HomeWork4Part1/ViewController.swift
@@ -12,13 +12,20 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var myLabel: UILabel!
     
+    @IBOutlet weak var myButtonOutlet: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         myLabel.text = "Edited text"
-
+        myButtonOutlet.titleLabel?.text = "Edited button text"
     }
 
+    @IBAction func myButtonAction(_ sender: Any) {
+        myLabel.text = "You pressed the button"
+
+    }
     
 }
 
