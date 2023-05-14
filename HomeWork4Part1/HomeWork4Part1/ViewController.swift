ead0099cacbcb7656d7d5d5896c5b4db73a48cd8
Daniil <45711483+DanHerasymenko@users.noreply.github.com>
2023-05-14 14:42:29 +0300

    added and edited label text

--- HomeWork4Part1/HomeWork4Part1/ViewController.swift
+++ HomeWork4Part1/HomeWork4Part1/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        myLabel.text = "Edited text"
+
     }
 
-
+    
 }
 
